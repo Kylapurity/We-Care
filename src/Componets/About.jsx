2397bac14c26143fc88472b29f5f3c2e55a5e992
pkihiu <p.kihiu@alustudent.com>
2024-03-26 16:07:58 +0200

    add about componets

--- src/Componets/About.jsx
+++ src/Componets/About.jsx
@@ -5,11 +5,11 @@ import Card from './Card';
 
 const About = () => {
   return (
-    <div className="container mx-auto" id='about'>
-      <h1 className="text-3xl font-bold mt-6 uppercase" href="#about">About Us</h1>
-      <div className="grid grid-cols-2 gap-4">
+    <div className="container mx-auto ">
+      <h1 className="text-3xl font-bold mt-6 uppercase underline text-blue-600">About Us</h1>
+      <div className="flex flex-wrap gap-6 ml-20 mt-5 m-20">
         <Card icon={<FaUser />} title="Our Team">
-        <p>Our team consists of experienced professionals from various fields who are passionate about making a positive impact on the lives of young individuals.</p>
+          <p>Our team consists of experienced professionals from various fields who are passionate about making a positive impact on the lives of young individuals.</p>
         </Card>
         <Card icon={<FaComments />} title="Our Mission">
           <p>To Save our Youths from Been lost with lack of skills and Know how to be ventures of saving the Economy</p>
@@ -18,7 +18,7 @@ const About = () => {
           <p>We will be offeering mentorships, Enterprnuers skill, Online class to advance their skills and alos we will be offeering first seed money to every venture.</p>
         </Card>
         <Card icon={<FaSmile />} title="Easy To use">
-          <p>To get acces to our platform we  the students will be paying $1 per month and if they cant access it we will be offeering a basic free</p>
+          <p>To get acces to our platform we  the students will be paying $1 per month and if they cant access it we will be offeering a basic free subcription.</p>
         </Card>
       </div>
     </div>
@@ -26,5 +26,7 @@ const About = () => {
 };
 
 export default About;
+
+
 
 
--- src/Componets/Card.jsx
+++ src/Componets/Card.jsx
@@ -4,16 +4,14 @@ import React from 'react';
 
 const Card = ({ icon, title, children }) => {
   return (
-    <div className="p-6 rounded-lg shadow-md">
-      <div className="flex items-center justify-center mb-4 text-blue-600">
-        {icon}
-      </div>
-      <h2 className="text-2xl font-semibold mb-2 underline">{title}</h2>
-      <div className="text-gray-700">
-        {children}
-      </div>
+    <div className="p-6 bg-gray-100 rounded-lg shadow-md w-72">
+      <div className="flex items-center justify-center mb-4 text-blue-600">{icon}</div>
+      <h2 className="text-3xl font-semibold mb-2 underline">{title}</h2>
+      <div className="text-black text-sm mt-5">{children}</div>
     </div>
   );
 };
 
 export default Card;
+
+
