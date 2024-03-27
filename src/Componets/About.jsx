78a0038585d6eeb4194c0217970a3993199b1de8
pkihiu <p.kihiu@alustudent.com>
2024-03-27 11:54:35 +0200

    add about componets

--- src/Componets/About.jsx
+++ src/Componets/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
     <div className="container-image">
       <img src="/src/Images/girl.jpg" alt="Women together" className="image" />
       <div className="max-w-md">
-        <p className="text-sm text-gray-800 mt-5">
+        <p className="text-1xl text-gray-800 mt-5">
           At our organization, we are dedicated to empowering the youth by providing them with the necessary skills and knowledge to thrive in today's world. Through our programs and initiatives, we aim to equip young individuals with practical skills in various fields, including technology, entrepreneurship, and personal development. We believe in the power of education and mentorship to unlock the potential of every young person. By offering mentorship programs, online courses, and workshops, we help aspiring entrepreneurs and professionals learn valuable skills, navigate challenges, and pursue their passions with confidence.
         </p>
       </div>
