30f379f5bab8105c0b50d804a99fa4e7fae2e2f3
pkihiu <p.kihiu@alustudent.com>
2024-04-10 22:22:34 +0200

    update Headear and courses

--- src/Componets/Header.jsx
+++ src/Componets/Header.jsx
@@ -3,22 +3,22 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <nav className="container mx-auto shadow-md py-2 px-10 md:py-3 md:px-5 pt-10">
+        <nav className="container mx-auto shadow-md py-2 px-10 md:py-3 md:px-5 pt-10" id='home'>
             <div className="flex items-center justify-center space-x-8"> 
                 <div className='flex'>
                    <h1 className='text-3xl text-blue-600 uppercase'>
                     <span  className="border-2 border-blue-600 rounded-full p-1">We</span>Care</h1>
                 </div>
                 <div className="hidden space-x-6 md:flex justify-between">
-                    <a href="#Hero" className="text-1.5xl text-black hover:text-gray-900 hover:underline active:underline font-bold uppercase">Home</a>
+                    <a href="#home" className="text-1.5xl text-black hover:text-gray-900 hover:underline active:underline font-bold uppercase">Home</a>
                     <span className="mx-4"></span>
-                    <a href="#menu" className="text-1xl text-black hover:text-gray-900 hover:underline active:underline font-bold uppercase">About</a>
+                    <a href="#about" className="text-1xl text-black hover:text-gray-900 hover:underline active:underline font-bold uppercase">About</a>
                     <span className="mx-4"></span>
                     <a href="#Courses" className="text-1xl text-black hover:text-gray-900 hover:underline active:underline font-bold uppercase">Courses</a>
                     <span className="mx-4"></span>
                     <a href="#testimonials" className="text-1xl text-black hover:text-gray-900 hover:underline active:underline font-bold uppercase">Testimonials</a>
                     <span className="mx-4"></span>
-                    <a href="#Contact" className="text-1xl text-black hover:text-gray-900 hover:underline active:underline font-bold uppercase">Contact Us</a>
+                    <a href="#contact" className="text-1xl text-black hover:text-gray-900 hover:underline active:underline font-bold uppercase">Contact Us</a>
                 </div>
                 <a href="#" className="hidden py-2 px-4 text-lg text-white bg-blue-600 rounded-full baseline hover:bg-darkBlue md:block uppercase">Get Started</a>
                 <button id="menu-btn" className="block md:hidden border-black focus:outline-none">
