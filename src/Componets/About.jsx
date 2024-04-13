74ad0bdeaa322ece6997f7d07643105c174bf1d3
pkihiu <p.kihiu@alustudent.com>
2024-04-13 12:28:47 +0200

    update about

--- src/Componets/About.jsx
+++ src/Componets/About.jsx
@@ -5,13 +5,14 @@ const About = () => {
   return (
     <div className='row align-items-xl-center gy-5'>
       <div className='col-xl-5 content pt-5 p-20'>
-        <h3 className='font-bold text-xl'>About US</h3>
-        <h2>Empowering Women with tech skills to be their own boss.</h2>
-        <p>Welcome to WeCar ean online education platform dedicated to empowering women with essential skills to become entrepreneurs and leaders in their own right.<br/> 
+        <h3 className='font-bold text-sm uppercase'>About US</h3>
+        <h2 className='font-bold text-3xl text-darkBlue uppercase '>Empowering Women with tech skills to be their own boss.</h2>
+        <p className='pt-4 font-medium'>Welcome to WeCar ean online education platform dedicated to empowering women with essential skills to become entrepreneurs and leaders in their own right.<br/> 
           At WeCare, we understand the importance of providing women with the tools and knowledge they need to thrive in todays ever-evolving world. <br/>
           We chose to focus on empowering women because we recognize the immense potential and talent that often goes untapped due to systemic barriers and societal norms. <br/>
           By equipping women with the skills and resources they need to succeed, we aim to break down these barriers and create a more equitable and inclusive society. <br/>
           </p>
+          <a href="">Watch More</a>
       </div>
       
     </div>
