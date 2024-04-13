6beba9528f984ae3e3ca6d69027ee3c22b81f6cd
pkihiu <p.kihiu@alustudent.com>
2024-04-13 11:46:04 +0200

    about.jsx

--- src/Componets/About.jsx
+++ src/Componets/About.jsx
@@ -1,26 +1,16 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React from 'react'
 
 const About = () => {
   return (
-
-    <div className="container-image">
-      <h1 className="flex justify-center text-blue-600 text-3xl">About us</h1>
-      <img src="/src/Images/girl.jpg" alt="Women together" className="image" />
-      <div className="max-w-md">
-        <p className="text-1xl text-gray-800 mt-5">
-          At our organization, we are dedicated to empowering the youth by providing them with the necessary skills and knowledge to thrive in today world. Through our programs and initiatives, we aim to equip young individuals with practical skills in various fields, including technology, entrepreneurship, and personal development. 
-        </p>
-        <button  type="button" onClick={() => window.location.href = '/signup'} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5">
-          Lets get started
-        </button>
+    <div className='row align-items-xl-center gy-5'>
+      <div className='col-xl-5 content'>
+        <h3>About US</h3>
+        <h2>We  care is a online education platform, where we provide women with skills and empower them vetures skills where they would be their own boos and enetrpernuesr</h2>
       </div>
+      
     </div>
-  );
+  )
 }
 
-export default About;
-
-
-
-
+export default About
