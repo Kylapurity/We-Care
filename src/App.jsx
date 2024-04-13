a48e0683fcd0d3e2ae1d59a906e522a145a3ff40
pkihiu <p.kihiu@alustudent.com>
2024-04-13 12:01:32 +0200

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import About from './Componets/About'
+import About from './Componets/About'
 import Courses from './Componets/Courses'
 import Footer from './Componets/Footer'
 import Header from './Componets/Header'
@@ -6,14 +6,14 @@ import Hero from './Componets/Hero'
 import Testimonials from './Componets/Testimonials'
 function App() {
   return (
-      <div>
-        < Header />
-        < Hero />
-        <Courses/>
-        <Testimonials/>
-        <Footer/>
-        {/* <About/>  */}
-      </div>
+    <div>
+      < Header />
+      < Hero />
+      <About />
+      <Courses />
+      <Testimonials />
+      <Footer />
+    </div>
   )
 }
 
