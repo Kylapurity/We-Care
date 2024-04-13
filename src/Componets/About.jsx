447809f6bf923ba526364ea04e53c0168d277cf2
pkihiu <p.kihiu@alustudent.com>
2024-04-13 12:00:09 +0200

    about.jsx

--- src/Componets/About.jsx
+++ src/Componets/About.jsx
@@ -4,9 +4,14 @@ import React from 'react'
 const About = () => {
   return (
     <div className='row align-items-xl-center gy-5'>
-      <div className='col-xl-5 content'>
-        <h3>About US</h3>
-        <h2>We  care is a online education platform, where we provide women with skills and empower them vetures skills where they would be their own boos and enetrpernuesr</h2>
+      <div className='col-xl-5 content pt-5 p-20'>
+        <h3 className='font-bold text-xl'>About US</h3>
+        <h2>Empowering Women with tech skills to be their own boss.</h2>
+        <p>Welcome to WeCar ean online education platform dedicated to empowering women with essential skills to become entrepreneurs and leaders in their own right.<br/> 
+          At WeCare, we understand the importance of providing women with the tools and knowledge they need to thrive in todays ever-evolving world. <br/>
+          We chose to focus on empowering women because we recognize the immense potential and talent that often goes untapped due to systemic barriers and societal norms. <br/>
+          By equipping women with the skills and resources they need to succeed, we aim to break down these barriers and create a more equitable and inclusive society. <br/>
+          </p>
       </div>
       
     </div>
